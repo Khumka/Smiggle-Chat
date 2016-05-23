bd079a20778e65abb9eae9ddee9e5a57969af182
Khumka <Khumka@users.noreply.github.com>
2016-05-23 15:27:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to your Web App</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='https://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+
+<br>
+<marquee> <h1> Welcome to Smiggle Chat &nbsp &nbsp &nbsp &nbsp Welcome to Smiggle Chat </h1></marquee> 
+<br><br>
+ 
+      <span><a href="login.php"><u><i>Sign in</u></a></span> <span> or <a href="register.php"><u>Sign up</a></span></u></i><br><br>
+<center>				
+<address style="background-color:lightblue;">		
+<p ><b>Smiggle Chat</b> Keeps you incontact with your loved ones,<br> people around the global are moving<br> forward with innovation from word of mouth.<br> This app is however a good result of that.</p>
+</address>
+</center>
+<img src="sihle.jpg" alt="audi.html width"600" height="200"/>
+<marquee style="background-color:lightblue;"direction="right"><h5><i> Designed by David K <sup> <mark>the Developer</mark> </i></marquee></sup>
+</body>
+</html>
